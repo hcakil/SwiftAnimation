fd51c62b5f05b929597ff414adf90d3bf44d7d32
hcakil <cakilhazim@gmail.com>
2022-06-06 21:55:07 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Labor Bilişim on 6.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
